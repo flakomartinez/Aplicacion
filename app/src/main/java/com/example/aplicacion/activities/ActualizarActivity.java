9443c942bf1f8db5ee31dc3245cfffd3a1ef76fd
yadira0 <yadira0@users.noreply.github.com>
2019-10-18 15:22:22 -0500

    arreglo login

---
+++ app/src/main/java/com/example/aplicacion/activities/ActualizarActivity.java
@@ -0,0 +1,16 @@
+package com.example.aplicacion.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aplicacion.R;
+
+public class ActualizarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar);
+    }
+}
